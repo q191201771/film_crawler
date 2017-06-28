84451283e7dd10b5755ced6fa7d2a304c8214c2f
q191201771 <191201771@qq.com>
2017-06-28 12:37:40 +0800

    使用selenium获取dy2018动态js生成的cookies

--- film_crawler/crawler/dy2018.py
+++ film_crawler/crawler/dy2018.py
@@ -3,8 +3,11 @@
 # @author chef <191201771@qq.com>
 # @brief  xxx
 
+import time
+
 import requests
 from bs4 import BeautifulSoup
+from selenium import webdriver
 
 from film_crawler.base import logger
 from film_crawler.data import FilmDownloadInfo
@@ -12,6 +15,21 @@ from .base import FilmDownloadInfoCrawler
 
 
 class Dy2018Crawler(FilmDownloadInfoCrawler):
+    def __init__(self):
+        FilmDownloadInfoCrawler.__init__(self)
+        logger.info('> 初始化dy2018.com验证信息...')
+        driver = webdriver.Firefox()
+        driver.get('http://www.dy2018.com')
+        time.sleep(5)
+        cookie_list = driver.get_cookies()
+        self.cookies = {}
+        for cookie in cookie_list:
+            self.cookies[cookie['name']] = cookie['value']
+        self.headers = {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:54.0) Gecko/20100101 Firefox/54.0'
+        }
+        logger.info('< 初始化dy2018.com验证信息完成.')
+
     def crawl(self, name):
         """@return FilmDownloadInfo list"""
         film_download_info_list = []
@@ -24,7 +42,7 @@ class Dy2018Crawler(FilmDownloadInfoCrawler):
         }
 
         try:
-            resp_content = requests.post(url, data=data, timeout=60).content.decode('gbk')
+            resp_content = requests.post(url, data=data, timeout=60, cookies=self.cookies, headers=self.headers).content.decode('gbk')
 
             soup = BeautifulSoup(resp_content, 'html.parser')
             items = soup.select('.co_content8 a.ulink')
